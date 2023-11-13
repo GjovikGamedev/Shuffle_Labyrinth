e78cbbdec1ecfd0f87ce92d710aed459f722524f
Jonekulhet <lervikjone@gmail.com>
2023-11-13 20:13:08 +0100

    Made a kinda functioning grid
    I can put new rooms in there nor problem. Is gonna need some more functionality.

--- Shuffle_Labyrinth/Assets/Scripts/levelScript.cs
+++ Shuffle_Labyrinth/Assets/Scripts/levelScript.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class levelScript : MonoBehaviour
 {
     public GameObject[] roomList;
-    public GameObject[][] grid;     //A 5 by 5 grid (0-4).
+    private GameObject[,] grid;     //A 5 by 5 grid (0-4).
+
+    [SerializeField] private int size;      //The length and height of the grid
 
     // Start is called before the first frame update
     void Start()
     {
-        //grid[0][0] = 
-        Debug.Log(GameObject.Find("Room_5"));
+        grid = new GameObject[size-1, size-1];        //Sets the length of the grid
+        grid[0, 0] = Instantiate(roomList[0], new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
