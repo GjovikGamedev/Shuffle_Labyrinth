90728fefa101b7734d2f0fddf98338c894b70d28
Jonekulhet <lervikjone@gmail.com>
2023-11-14 15:22:19 +0100

    Finished Move() for moveScript (not for edge cases)

--- Shuffle_Labyrinth/Assets/Scripts/roomScript.cs
+++ Shuffle_Labyrinth/Assets/Scripts/roomScript.cs
@@ -5,33 +5,59 @@ using UnityEngine;
 public class roomScript : MonoBehaviour
 {
     float moveTime;
+    float numSteps;
+    float newX;
+    float newZ;
+    bool moving;
     // Start is called before the first frame update
     void Start()
     {
         //GameObject theLevel = level;
         moveTime = GameObject.Find("Level").GetComponent<levelScript>().moveTime;
+        numSteps = moveTime / Time.deltaTime;
+        moving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (moving)
+        {
+            transform.position += new Vector3(              //Changes the position
+                (newX - transform.position.x) / numSteps,     //The change in x-position for each step
+                0,
+                (newZ - transform.position.z) / numSteps);
+
+            if ((newX - transform.position.x) < 0.1 && (newZ - transform.position.z) < 0.1)
+            {
+                transform.position = new Vector3 (newX, 0, newZ);
+                moving = false;
+            }
+        }
+
     }
 
     //Moves this room from it's current position to the new position described by the 
-    public void Move(float newX, float newZ)
+    public void Move(float veryNewX, float veryNewZ)
     {
-        float numSteps = moveTime/Time.deltaTime;           //How many steps the room is gonna take in order to be done by moveTime
+        /*float numSteps = moveTime/Time.fixedDeltaTime;           //How many steps the room is gonna take in order to be done by moveTime
         for (int i = 0; i < numSteps; i++)   //Should I be using fixed deltatime for numSteps?
         {
+            Debug.Log("Yay");
             transform.position += new Vector3(              //Changes the position
                 (newX-transform.position.x) / numSteps,     //The change in x-position for each step
                 0,
                 (newZ-transform.position.z) / numSteps);    //The change in z-position for each step
         }
 
+        Debug.Log(numSteps);
         transform.position = new Vector3(newX, 0, newZ);    //Moves it into place properly at the end. This is more of a
                                                             //precaution, in case some rounding errors or something happened in the loop
+        */
+        newX = veryNewX;
+        newZ = veryNewZ;
+        moving = true;
     }
 
     //Different function for the edge cases (when a room needs to get to the other side of the grid)
