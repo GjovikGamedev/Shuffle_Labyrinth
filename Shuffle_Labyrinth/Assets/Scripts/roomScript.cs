48409a16f102d6ddd1815fbb0a2f61dd1a43b78d
Jonekulhet <lervikjone@gmail.com>
2023-11-14 14:18:51 +0100

    Added moveTime to level
    moveTime is a float that is how many seconds its gonna take from when the shuffling of a room starts, until it's in it's new position.

--- Shuffle_Labyrinth/Assets/Scripts/roomScript.cs
+++ Shuffle_Labyrinth/Assets/Scripts/roomScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class roomScript : MonoBehaviour
 {
+    float moveTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GameObject theLevel = level;
+        moveTime = GameObject.Find("Level").GetComponent<levelScript>().moveTime;
+        Debug.Log(moveTime);
     }
 
     // Update is called once per frame
@@ -19,7 +22,7 @@ public class roomScript : MonoBehaviour
     //Moves this room from it's current position to the new position described by the 
     void Move(float newX, float newY)
     {
-
+        //for (int i = 0; i < )
     }
 
     //Different function for the edge cases (when a room needs to get to the other side of the grid)
