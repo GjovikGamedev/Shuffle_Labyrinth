d813888e6c687227379c697e28726499831ac9e8
Jonekulhet <lervikjone@gmail.com>
2023-11-14 13:14:11 +0100

    Rooms for the level now spawn in
    For which rooms spawned in, I just cycled through the types over and over.

--- Shuffle_Labyrinth/Assets/Scripts/levelScript.cs
+++ Shuffle_Labyrinth/Assets/Scripts/levelScript.cs
@@ -19,11 +19,24 @@ public class levelScript : MonoBehaviour
         grid = new GameObject[gridSize, gridSize];        //Sets the length of the grid
 
         int numRooms = roomList.Length;               //The number of unique rooms
-        int whichRoom = 0;                            //Used to cycle through rooms in the following loop
+        int roomType = 0;                            //Used to cycle through rooms in the following loop
         for (int i = 0; i < gridSize; i++)
         {
             for(int j = 0; j < gridSize; j++) {
-                grid[j, i] = Instantiate(roomList[0], new Vector3(0, 0, 0), Quaternion.identity);
+                //Should probably append the instantiated objects to the level object sometime
+                grid[j, i] = Instantiate(roomList[roomType], new Vector3(0, 0, 0), Quaternion.identity);
+
+                grid[j, i].transform.Rotate(-90, 0, 0);         //Rotates the rooms to fit in with the top-down perspective. The prefabs are actually already rotated, but for some reason it doesn't apply to these instantiated ones
+                //Places the rooms evenly in the grid
+                grid[j, i].transform.position = new Vector3(j*roomLength, 0, i*roomLength);
+
+                Debug.Log(roomType);
+                if (++roomType >= numRooms)          //Goes to the next room in the roomList. If outside roomlist, then go to the beginning of roomlist
+                {
+                    roomType = 0;
+                }
+                Debug.Log(roomType);
+                Debug.Log("-----------");
             }
         }
         //grid[0, 0] = Instantiate(roomList[0], new Vector3(0, 0, 0), Quaternion.identity);
