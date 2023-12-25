5022fe1bc72f85e0979cc6c046c63a9cfd062e36
Jonekulhet <lervikjone@gmail.com>
2023-12-26 00:33:29 +0100

    Implemented the variable "facing"
    Facing keeps track of which of the four directions the player faces.

--- Shuffle_Labyrinth/Assets/Scripts/playerController.cs
+++ Shuffle_Labyrinth/Assets/Scripts/playerController.cs
@@ -14,6 +14,9 @@ public class playerController : MonoBehaviour
     float sprintSpeed;
     Rigidbody rb;
 
+    int facing;             //The direction the player is facing. 1 == right | 2 == left | 3 == up | 4 == down
+    Vector3 mDirection;     //The direction the rooms are going to shift
+
     public bool gameIsPaused = false;
     public GameObject menuObject;
 
@@ -55,6 +58,44 @@ public class playerController : MonoBehaviour
             {
                 Pause();
             }
+        }
+
+        //Determines the direction of the player
+        if (Mathf.Abs( Input.GetAxis("Horizontal") ) > Mathf.Abs( Input.GetAxis("Vertical") )       //The player is going more to the side than up/down
+            && Mathf.Abs( Input.GetAxis("Horizontal")) > 0)                                         //and is going to the side
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                facing = 1;     //Player is facing right
+            }
+            else
+            { 
+                facing = 2;     //Player is facing left
+            }
+
+        //If the input up/down is larger than the input to the sides, then the 'else if' sentence is triggered
+        } else if (Mathf.Abs( Input.GetAxis("Vertical") ) > 0)   //If the player is moving up or down
+        {
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                facing = 3;     //Player is facing up
+            }
+            else
+            {
+                facing = 4;     //Player is facing down
+            }
+        }
+
+        /*
+        if ()
+        {
+
+        }*/
+
+        //Some testing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(facing);
         }
     }
 
